8981587c6128c17bf15a1583aaf707b50b2ee7e2
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-05-26 11:12:07 +0200

    demo

--- rule.tf
+++ rule.tf
@@ -1,3 +1,12 @@
+resource "elasticstack_kibana_action_connector" "my_index_connector" {
+  name              = "index-connector-1"
+  connector_type_id = ".index"
+
+  config = jsonencode({
+    index = ".test-index"
+  })
+}
+
 resource "elasticstack_kibana_alerting_rule" "my_rule" {
   name         = "rule 1"
   consumer     = "alerts"
@@ -30,47 +39,4 @@ resource "elasticstack_kibana_alerting_rule" "my_rule" {
       }]
     })
   }
-}
-
-resource "elasticstack_kibana_alerting_rule" "my_second_rule" {
-  name         = "rule 2"
-  consumer     = "alerts"
-  rule_type_id = ".index-threshold"
-  notify_when  = "onActionGroupChange"
-  interval     = "1m"
-  enabled      = true
-
-  params = jsonencode({
-    aggType             = "avg"
-    termSize            = 6
-    thresholdComparator = ">"
-    timeWindowSize      = 5
-    timeWindowUnit      = "m"
-    groupBy             = "top"
-    threshold           = [1000]
-    index               = [".test-index"]
-    timeField           = "@timestamp"
-    aggField            = "sheet.version"
-    termField           = "name.keyword"
-  })
-
-  actions {
-    id    = elasticstack_kibana_action_connector.my_index_connector.connector_id
-    group = "threshold met"
-    params = jsonencode({
-      "documents" : [{
-        "rule_id" : "{{rule.id}}",
-        "alert_id" : "{{alert.id}}"
-      }]
-    })
-  }
-}
-
-resource "elasticstack_kibana_action_connector" "my_index_connector" {
-  name              = "index-connector-1"
-  connector_type_id = ".index"
-
-  config = jsonencode({
-    index = ".test-index"
-  })
 }
